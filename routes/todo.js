0d9ec632fbcd7cfe294edb3f4648bdc9566ef499
Emad Albalawi <91331130+iemad00@users.noreply.github.com>
2025-02-05 20:23:26 +0300

    get todos

--- routes/todo.js
+++ routes/todo.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
+// In-memory storage for to-do items
+const todos = [];
+
+// Get all to-do items
 router.get("/", (req, res) => {
-    res.send("Hello World");
+    res.status(200).json(todos);
 });
 
 
