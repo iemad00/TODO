44bb5935813db997011efa664c4c72c4c595975d
Emad Albalawi <91331130+iemad00@users.noreply.github.com>
2025-02-05 21:12:02 +0300

    Using Global Responses with refactoring

--- middlewares/validateSchema.js
+++ middlewares/validateSchema.js
@@ -1,10 +1,12 @@
 
+const { BadRequest } = require("../utils/responseHelper");
+
 // This middleware is used to validate the schema of the request body
 function validateSchema(schemaValidator) {
     return (req, res, next) => {
         const valid = schemaValidator(req.body);
         if (!valid) {
-            return res.status(400).json({ errors: schemaValidator.errors });
+            return BadRequest(res, "Invalid request body", schemaValidator.errors);
         }
         next();
     };
--- routes/todo.js
+++ routes/todo.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const { v4: uuidv4 } = require("uuid");
+const { OK, Created, NotFound } = require("../utils/responseHelper");
 const validateSchema = require("../middlewares/validateSchema");
 const { validateCreateTodo, validateUpdateTodo } = require("../schemas/todo");
 
@@ -8,16 +9,14 @@ const { validateCreateTodo, validateUpdateTodo } = require("../schemas/todo");
 const todos = [];
 
 // Get all to-do items
-router.get("/", (req, res) => {
-    res.status(200).json(todos);
-});
+router.get("/", (req, res) => OK(res, "To-do list fetched", todos));
 
 // Add a new to-do item
 router.post("/", validateSchema(validateCreateTodo), (req, res) => {
     const { title, completed } = req.body;
     const newTodo = { id: uuidv4(), title, completed };
     todos.push(newTodo);
-    res.status(201).json(newTodo);
+    return Created(res, "To-do item created", newTodo);
 });
 
 // Update a to-do item (partial updates)
@@ -26,30 +25,22 @@ router.patch("/:id", validateSchema(validateUpdateTodo), (req, res) => {
     const { title, completed } = req.body;
 
     const todo = todos.find((t) => t.id === id);
-    if (!todo) {
-        return res.status(404).json({ error: "To-do item not found." });
-    }
+    if (!todo) return NotFound(res, "To-do item not found");
 
-    if (title !== undefined) {
-        todo.title = title;
-    }
+    if (title !== undefined) todo.title = title;
+    if (completed !== undefined) todo.completed = completed;
 
-    if (completed !== undefined) {
-        todo.completed = completed;
-    }
-
-    res.status(200).json(todo);
+    return OK(res, "To-do item updated", todo);
 });
 
 // Delete a to-do item
 router.delete("/:id", (req, res) => {
     const { id } = req.params;
     const index = todos.findIndex((t) => t.id === id);
-    if (index === -1) {
-        return res.status(404).json({ error: "To-do item not found." });
-    }
+    if (index === -1) return NotFound(res, "To-do item not found");
+
     todos.splice(index, 1);
-    res.status(204).send();
+    return OK(res, "To-do item deleted");
 });
 
 module.exports = router;
