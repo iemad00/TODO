692c4f5bdb61b6f4fe73423217ca085d3cd3cc84
Emad Albalawi <91331130+iemad00@users.noreply.github.com>
2025-02-05 20:27:43 +0300

    Create TODO

--- routes/todo.js
+++ routes/todo.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const { v4: uuidv4 } = require("uuid");
 
 // In-memory storage for to-do items
 const todos = [];
@@ -7,6 +8,14 @@ const todos = [];
 // Get all to-do items
 router.get("/", (req, res) => {
     res.status(200).json(todos);
+});
+
+// Add a new to-do item
+router.post("/", (req, res) => {
+    const { title, completed } = req.body;
+    const newTodo = { id: uuidv4(), title, completed };
+    todos.push(newTodo);
+    res.status(201).json(newTodo);
 });
 
 
